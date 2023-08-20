0026466b6c9871565576d1a23e2db7c52f5275bb
Mitchbux <55109545+Mitchbux@users.noreply.github.com>
2023-08-20 23:04:06 +0200

    Even faster : 1m36

--- fastwin32.c
+++ fastwin32.c
@@ -131,7 +131,7 @@ int zsx_lf(unsigned long n) {
 	return m;
 }
 
-#define zsx_chunk_bits 17
+#define zsx_chunk_bits 24
 #define zsx_chunk (1<<zsx_chunk_bits)
 #define zsx_bits 8
 #define zsx_half 4
@@ -158,11 +158,10 @@ byte *zsx_encode(byte *data) {
 	int *initial = zsx_new(int, zsx_top);
 	int *indexes = zsx_new(int, zsx_top);
 	int *idx = zsx_new(int, zsx_top);
-	int *bad = zsx_new(int, zsx_top);
-	int id , in = 0, next = 0, last = 0, best = 0;
+	int id , in = 0, next = 0, last = zsx_values-1, best = 0;
 	
 	for(int s=0;s<zsx_top;s++)
-		bad[s] = 0, idx[s]=0, indexes[s]=0;
+		idx[s]=0, indexes[s]=0;
 	
 	while (in < len ) {
     int z = data[in++] , s = data[in++], x = data[in++], hash = (z<<zsx_bits) ^(s << zsx_half) ^ x;
@@ -172,30 +171,30 @@ byte *zsx_encode(byte *data) {
 			{
 				zsx_write_bit(bytes,1);
 				zsx_write_value(bytes, idx[hash], zsx_lf(last));
-				if(idx[hash]<256)
+				if(idx[hash]<zsx_values)
 				if(indexes[idx[hash]])
 				{
 					zsx_write_value(bytes, hash, zsx_bits+zsx_bits);
 					indexes[idx[hash]]=0;
 					idx[hash]=++last;
 				}
+				zsx_write_bit(bytes, initial[hash] != s ? 1 : 0);
+				if (initial[hash] != s){ initial[hash] = s;
+					zsx_write_value(bytes, s, zsx_bits);}		
 			}
 			else
 			{
 				zsx_write_bit(bytes,0);
-				s = last==255?++next:++last;
-				if(next==255) next = 0;
+				initial[hash] = s;
 				if(indexes[s])
 				{
-					zsx_write_value(bytes, indexes[s], zsx_bits+zsx_bits);
-					idx[indexes[s]]=0;
+					zsx_write_value(bytes, indexes[s]-1, zsx_bits+zsx_bits);
+					idx[indexes[s]-1]=0;
 				}
 				idx[hash]=s;
-				indexes[s]=hash;
+				indexes[s]=hash+1;
 			}
-				zsx_write_bit(bytes, initial[hash] != s ? 1 : 0);
-				if (initial[hash] != s){ initial[hash] = s;
-					zsx_write_value(bytes, s, zsx_bits);}		
+					
 	}
 	
 	
@@ -207,7 +206,6 @@ byte *zsx_encode(byte *data) {
 	zsx_del(initial);
 	zsx_del(indexes);
 	zsx_del(idx);
-	zsx_del(bad);
 	
 	zsx_del(reader);
 	zsx_del(bytes);
@@ -336,8 +334,6 @@ int test(char *filename) {
 
 	fclose(f);
 	CloseHandle(hFile);
-
-	//CloseCompressor(Compressor);
 
 	//*** re-encoding the whole file to a minimum set of data ***
 
