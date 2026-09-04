794f4a86781b338ac7da33e3d189fac108d3eed8
Mitchbux <55109545+Mitchbux@users.noreply.github.com>
2026-09-04 11:41:41 +0200

    Add ZSXEncode.hx

---
+++ ZSXEncode.hx
@@ -0,0 +1,11 @@
+import Shrink;
+import haxe.io.Bytes;
+class ZSXEncode
+{	
+  static function main() {
+		var chunk:Bytes = Bytes.alloc(1024);
+		Shrink.encode(chunk);
+		ZsxRange.encodeChunk(Bytes.alloc(0));
+		ZsxArchive.encode(Bytes.alloc(0));
+	}
+}
